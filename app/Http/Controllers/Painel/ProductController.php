1350ad262fb13c3a2d493ed8087337eb60b6fea6
lucianoecunha <luno84@gmail.com>
2017-01-22 23:48:12 -0200

    Atualizaçao na controllerprodutos

--- app/Http/Controllers/Painel/ProductController.php
+++ app/Http/Controllers/Painel/ProductController.php
@@ -89,6 +89,8 @@ class ProductController extends Controller {
     public function testes(){
         
        /*
+        * 
+        * forma ruim...
         $prod = $this->product;
         $prod->name = 'nome do produto';
         $prod->number = 123456;
@@ -106,7 +108,7 @@ class ProductController extends Controller {
         else return 'falha ao inserir';
         
         /*/
-      
+      /* Melhor forma
         
       $insert = $this->product->create([
            'name' => 'nome do produto 2',
@@ -121,8 +123,83 @@ class ProductController extends Controller {
           return "inseriu blz id: {$insert->id}";
       }
       else 'não inseriu';
-               
-    }       
- 
+        */ 
+        
+        //atualiza menos produtivo
+       /*
+        $prod = $this->product->find(5);
+        $prod->name = 'produto id 5';
+        $prod->number = 123456;
+        $prod->image = 'abecdesdfs';
+        $prod->active = true;
+        $prod->categoria = 'eletornicos';
+        $prod->description = 'description do produtos';
+        
+         $update = $prod->save();
+        
+        if($update)
+        {
+            return 'atualizou ok';
+        }
+        else return 'falha ao atualizar';
+        * 
+        * o metodo dd() faz o dump.. e fino pq ja msotra o array
+        */
+       
+        
+        /*
+        $prod = $this->product->find(6);
+        $prod->update([
+           'name'       => 'nome do update teste 2',
+           'number'     => 123456789,
+           'active'     => TRUE,
+           'image'      => 'abcdesffs',
+           'categoria'  => 'cozinha'
+            
+        ]);
+        
+       //  $update = $prod->save();
+        
+        if($update)
+        {
+            return 'atualizou ok';
+        }
+        else return 'falha ao atualizar';
+      
+     */ 
+    //update com where no registro
+   /*
+        $update = $this->product
+                ->where('number', 123456)
+                // pode ter quantos where for necesssario
+                ->update([
+            'name' => 'nome do where',
+            'active' => TRUE,
+            'image' => 'abcdesffs'
+        ]);
+
+
+        if ($update) {
+            return 'atualizou ok';
+        } else
+            return 'falha ao atualizar';  */
+        
+        /* delete por ID
+        
+        $prod = $this->product->find(3);
+        $delete = $prod->delete();
+        
+         if ($delete) {
+            return 'deletou ok';
+        } else
+            return 'falha ao deletar';
+        */
+         $delete = $this->product->where('number',123456)->delete();
+          if ($delete) {
+            return 'deletou ok';
+        } else
+            return 'falha ao deletar';
+        
+    }
 
 }
